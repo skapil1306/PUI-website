98194d9672da67ae6810dbc909ae55a9144f959d
Swarnakshi Kapil <skapil@andrew.cmu.edu>
2018-11-30 22:24:06 -0500

    Adding all final files

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,30 @@ window.onload = function() {
         }
     }
     
-    //PROJECTS PAGE
-    
+    //PROJECTS PAGE FILTER
+    var Portfolio = {
+        sort: function(items) {
+            items.show();
+            $('#portfolio-content').find('div.portfolio-item').not(items).fadeOut(500);
+        },
+        showAll: function(items) {
+            items.fadeIn(500);
+        },
+        //sorting items based on their identifier
+        doSort: function() {
+            $('a', '#portfolio-sort').on('click', function() {
+                var $a = $(this);
+                if (!$a.is('#all')) {
+                    var items = $('div[data-cat=' + $a.data('cat') + ']', '#portfolio-content');
+                    Portfolio.sort(items);
+                } 
+                else {
+                    Portfolio.showAll($('div.portfolio-item', '#portfolio-content'));
+                }
+            });
+        }
+    };
+
+    Portfolio.doSort();
     
 }
